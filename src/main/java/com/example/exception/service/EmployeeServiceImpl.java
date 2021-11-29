8341808861fff66ba2b463596c7da052bd87189b
Елена Хилькович <elenakhilkovich@gmail.com>
2021-11-29 23:34:47 +0300

    исправила, lesson 2_6

--- src/main/java/com/example/exception/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/exception/service/EmployeeServiceImpl.java
@@ -6,41 +6,39 @@ import com.example.exception.model.Employee;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
-    private final ArrayList<Employee> employee = new ArrayList<>();
+    private final List<Employee> employee = new ArrayList<>();   //убрала ArraysList
 
     @Override
     public Employee addEmployee(String lastName, String firstName) {
-        Employee newEmployee = new Employee(lastName, firstName);//throws - чтобы внутри этого метода не обрабатывалось исключение
-        if (employee.size() < 3) {
-            employee.add(newEmployee);
-            return newEmployee;
-        } else {
-            throw new ArrayOverflowException();
-        }
+        Employee newEmployee = new Employee(lastName, firstName);
+
+        employee.add(newEmployee);
+        return newEmployee;
     }
 
     @Override
-    public Employee removeEmployee(String lastName, String firstName)  {    //throws - чтобы внутри этого метода не обрабатывалось исключение
-        Employee removeEmployee = new Employee(lastName, firstName);
-        if (employee.contains(removeEmployee)) {
-            employee.remove(removeEmployee);
-            return removeEmployee;
+    public Employee removeEmployee(String lastName, String firstName) {
+        Employee employeeToRemove = new Employee(lastName, firstName);
+        if (employee.contains(employeeToRemove)) {
+            employee.remove(employeeToRemove);
+            return employeeToRemove;
         } else {
             throw new NotFoundException();
         }
     }
 
     @Override
-    public Employee findEmployee(String lastName, String firstName)  {
+    public Employee findEmployee(String lastName, String firstName) {
 
-        Employee findEmp = new Employee(lastName, firstName);
+        Employee employeeToFind = new Employee(lastName, firstName);
         for (int i = 0; i < employee.size(); i++) {
-            if (employee.get(i).equals(findEmp)) {
-                return employee.get(i);
+            if (employee.get(i).equals(employeeToFind)) {
+                return employeeToFind;
             }
         }
         throw new NotFoundException();
