126d6a33a087eba3f83cd8f88342862b63493d42
Елена Хилькович <elenakhilkovich@gmail.com>
2021-12-26 22:54:53 +0300

    lesson 2_10

--- src/main/java/com/example/exception/controller/ExcController.java
+++ src/main/java/com/example/exception/controller/ExcController.java
@@ -1,7 +1,9 @@
 package com.example.exception.controller;
 
+import com.example.exception.exception.EmployeeBadRequestException;
 import com.example.exception.model.Employee;
 import com.example.exception.service.EmployeeService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,8 +19,17 @@ public class ExcController {
     }
 
     @GetMapping("/add")
-    public Employee addEmployee(@RequestParam String lastName, @RequestParam String firstName, @RequestParam int departmentId, @RequestParam int salary) {
-        return employeeService.addEmployee(lastName, firstName, departmentId, salary);
+    public Employee addEmployee(@RequestParam String lastName,
+                                @RequestParam String firstName,
+                                @RequestParam int departmentId,
+                                @RequestParam int salary) throws EmployeeBadRequestException {
+        if (StringUtils.isAlpha(lastName) && StringUtils.isAlpha(firstName)) {
+            lastName = StringUtils.capitalize(lastName);
+            firstName = StringUtils.capitalize(firstName);
+            return employeeService.addEmployee(lastName, firstName, departmentId, salary);
+        } else {
+            throw new EmployeeBadRequestException();
+        }
     }
 
     @GetMapping("/remove")
